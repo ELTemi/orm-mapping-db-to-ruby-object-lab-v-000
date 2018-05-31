38b2efc54dca4a0611a693904f5e5ff8d8c4a5d9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 18:55:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,11 +27,11 @@ class Student
       WHERE name = ?
       LIMIT 1
     SQL
-
+      binding.pry
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end
-    binding.pry
+  
     # find the student in the database given a name
     # return a new instance of the Student class
   end
