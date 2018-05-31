61011b42ce7adc3055f207477c0fd94ea5cb6654
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 19:47:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,6 +84,18 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end  
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
